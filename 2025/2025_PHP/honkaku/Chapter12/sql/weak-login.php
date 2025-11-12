92eb80e6cf1338daddc9b7b3a89bee223a77711f
YoshimasaKitano <gabugabu818@gmail.com>
2025-11-13 01:08:34 +0900

    11.13 1:08

--- 2025/2025_PHP/honkaku/Chapter12/sql/weak-login.php
+++ 2025/2025_PHP/honkaku/Chapter12/sql/weak-login.php
@@ -6,4 +6,52 @@
     function connect(): PDO
     {
         $pdo = new PDO('mysql:host=localhost; dbname=honkaku; charset=utf8mb4', 'root', "");
+        $pdo->setAttribte(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
     }
+
+    // ログインID・パスワードを元に一致するユーザを探す関数
+    function findUser(string $loginId, string $password): ?array
+    {
+        $pdo = connect();
+        $sql = "SELECT * FROM honkaku_users WHERE login_id = '{$loginId}' AND password = '{$password}'";
+        $statement = $pdo->query($sql);
+        $user = $statement->fetch(PDO::FETCH_ASSOC);
+        if ($user['id']) {
+            return $user;
+        }
+        return null;
+    }
+
+    // HTMLエスケープする関数
+    function escape(string $value): string
+    {
+        return htmlspecialchars($value, ENT_QUOTES | ENT_HTML5, 'UTF-8');
+    }
+
+    // メインルーチン
+    if (isset($_POST['login_id']) && isset($_POST['password'])) {
+        try {
+            $user = findUser($_POST['login_id'], $_POST['password']);
+        } catch (PDOException $e) {
+            echo 'Error';
+            exit;
+        }
+    }
+?>
+<body>
+    <h1>ログイン画面</h1>
+    <form name="login-form" action="" method="POST">
+        ID：<input type="text" name="login_id" value="">(例：dummy)<br>
+        パスワード：<input type="text" name="password" value="">(例：'or'1'='1)<br>
+        <button type="submit" name="operation" value="login">ログイン</button>
+    </form>
+    <hr>
+    <?php if (isset($_POST['operation']) && $_POST['operation'] === 'login'): ?>
+        <?php if (intval($user['id']) > 0): ?>
+            <p><?=escape($user['user_name'])?>さん。ログインに成功しました。
+        <?php else:?></p>
+            <p>ログインに失敗しました。</p>
+        <?php endif; ?>
+    <?php endif;?>
+</body>
