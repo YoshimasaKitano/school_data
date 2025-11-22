c9c0e93a64309fe1392b5264489e5a9275a4f69a
YoshimasaKitano <gabugabu818@gmail.com>
2025-11-22 23:45:02 +0900

    11.22 23:44

---
+++ 2025/2025_PHP/practice/test02_practice/practice1.php
@@ -0,0 +1,60 @@
+<?php
+    try {
+        $pdo = new PDO('mysql:host=localhost; dbname=honkaku; charset=utf8b4', 'root', '');
+
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        echo '接続に成功しました。';
+    } catch (PDOException $e) {
+        echo '接続に失敗しました。';
+    }
+?>
+
+
+<?php
+    try {
+        $pdo = new PDO('mysql:host=localhost; dbname=honkaku; charset=utf8mb4', 'root', '');
+
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ATTR_ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        echo '接続に成功しました。';
+    } catch (PDOException $e) {
+        echo '接続に成功しました。';
+    }
+?>
+
+<?php
+    try {
+        $pdo = new PDO('mysql:host=localhost; dbname=honkaku; charset=utf8mb4', 'root', '');
+
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ATTR_ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        echo '接続に成功しました。';
+    } catch (PDOException $e) {
+        echo '接続に失敗しました。';
+    }
+?>
+
+<?php
+    try {
+        $pdo = new PDO('mysql:host=localhost; dbname=honkaku; charset=utf8mb4', 'root', '');
+
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ATTR_ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        echo '接続に成功しました。';
+    } catch (PDOException $e) {
+        echo '接続に失敗しました。';
+    }
+?>
+
+<?php
+    try {
+        $pdo = new PDO('mysql:host=localhost; dbname=honkaku; charset=utf8mb4', 'root', '');
+
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ATTR_ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        echo '接続に成功しました。';
+    } catch (PDOException $e) {
+        echo '接続に失敗しました。';
+    }
+?>
