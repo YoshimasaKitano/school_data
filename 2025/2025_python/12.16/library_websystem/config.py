a64fad86e4e0f9fc257848346c22cfafc8e88102
YoshimasaKitano <gabugabu818@gmail.com>
2025-12-16 16:08:05 +0900

    12.16 16:04

---
+++ 2025/2025_python/12.16/library_websystem/config.py
@@ -0,0 +1,12 @@
+### config.py
+"""
+アプリケーションの設定を管理する
+"""
+# データベースの接続管理
+DB_CONFIG = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'Pa$$w0rd',
+    'database': 'lws_db',
+    'charset': 'utf8',
+}
