7fbbe6ad32bd27ec6b4d4f363cadeebb3e906db1
YoshimasaKitano <gabugabu818@gmail.com>
2025-12-16 23:54:21 +0900

    12.16 23:54

---
+++ 2025/2025_PHP/practice/test02_practice/practice4.php
@@ -0,0 +1,21 @@
+<?php
+    try {
+        $pdo = new PDO('mysql:host=localhost; dbname=honkaku; charset=utf8mb4', 'root', '');
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        echo '接続に成功しました。';
+    } catch (PDOException $e) {
+        echo '接続に失敗しました。';
+    }
+?>
+
+<?php
+    try {
+        $pdo = new PDO('mysql:host=localhost; dbname=honkaku; charset=utf8mb4', 'root', '');
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        echo '接続に成功しました';
+    } catch (PDOException $e) {
+        echo '接続に失敗しました';
+    }
+?>
